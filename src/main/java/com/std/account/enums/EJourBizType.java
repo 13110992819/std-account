9b301a1c19e26f4ccacb6012adea2699a8d6db46
haiqingzheng <leo.zheng@hichengdai.com>
2017-05-24 17:40:19 +0800

    姚橙：update

--- src/main/java/com/std/account/enums/EJourBizType.java
+++ src/main/java/com/std/account/enums/EJourBizType.java
@@ -29,7 +29,9 @@ public enum EJourBizType {
             "LTSFC", "量体师分成")
 
     // 姚橙
-    , YC_O2O("YC_O2O", "姚橙O2O支付"), YC_MALL("YC_MALL", "姚橙商城支付");
+    , YC_O2O("YC_O2O", "姚橙O2O支付"), YC_MALL("YC_MALL", "姚橙商城购物支付"), YC_MALL_BACK(
+            "YC_MALL_BACK", "姚橙商城购物退款"), YC_XNCZ_P("YC_XNCZ_P", "姚橙充值专区用款"), YC_XNCZ_M(
+            "-94", "菜狗充值专区退款");
 
     public static EJourBizType getBizType(String code) {
         Map<String, EJourBizType> map = getBizTypeMap();
