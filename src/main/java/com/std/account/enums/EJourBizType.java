89d81184b98b739cc5d2d1d15424c0a3827348cf
haiqingzheng <leo.zheng@hichengdai.com>
2017-05-26 16:16:24 +0800

    update

--- src/main/java/com/std/account/enums/EJourBizType.java
+++ src/main/java/com/std/account/enums/EJourBizType.java
@@ -37,7 +37,9 @@ public enum EJourBizType {
     // 姚橙
     , YC_O2O("YC_O2O", "姚橙O2O支付"), YC_MALL("YC_MALL", "姚橙商城购物支付"), YC_MALL_BACK(
             "YC_MALL_BACK", "姚橙商城购物退款"), YC_XNCZ_P("YC_XNCZ_P", "姚橙充值专区用款"), YC_XNCZ_M(
-            "YC_XNCZ_M", "姚橙充值专区退款"), YC_CBGM("YC_CBGM", "橙币购买");
+            "YC_XNCZ_M", "姚橙充值专区退款"), YC_CBGM("YC_CBGM", "橙币购买"), YC_SCB(
+            "YC_SCB", "扫描二维码送橙币");
+    ;
 
     public static EJourBizType getBizType(String code) {
         Map<String, EJourBizType> map = getBizTypeMap();
